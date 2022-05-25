f07e62ff71607ca5899eb616c8f60b0972d2c355
Edd Barrett <vext01@gmail.com>
2022-05-25 12:01:49 +0100

    Start work on stackmap tests.

---
+++ MultiSource/UnitTests/Stackmaps/StackMapParser.cpp
@@ -0,0 +1,116 @@
+#include <inttypes.h>
+
+#include <assert.h>
+#include <elf.h>
+#include <err.h>
+#include <fcntl.h>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
+#include "StackMapParser.h"
+
+using namespace std;
+
+template <typename T> T StackMapParser::read(char **SMData) {
+  T Ret = *reinterpret_cast<T *>(*SMData);
+  *SMData += sizeof(T);
+  return Ret;
+}
+
+void StackMapParser::alignTo(char **SMData, size_t To) {
+  // FIXME improve.
+  while (reinterpret_cast<uintptr_t>(*SMData) % To != 0)
+    (*SMData)++;
+}
+
+StackMapParser::StackMapParser(char *SMData) {
+  // Header.
+  uint8_t Version = read<uint8_t>(&SMData);
+  if (Version != SMVersion)
+    errx(EXIT_FAILURE, "expected stackmap version '%d', got version '%d'",
+         SMVersion, Version);
+  read<uint8_t>(&SMData);  // Skip reserved.
+  read<uint16_t>(&SMData); // Skip reserved.
+
+  // Multiplicities of tables.
+  uint32_t NumFunctions = read<uint32_t>(&SMData);
+  uint32_t NumConstants = read<uint32_t>(&SMData);
+  uint32_t NumRecords = read<uint32_t>(&SMData);
+
+  // Functions.
+  for (uint32_t I = 0; I < NumFunctions; I++)
+    Funcs.push_back({read<uint64_t>(&SMData), read<uint64_t>(&SMData),
+                     read<uint64_t>(&SMData)});
+
+  // Constants.
+  for (uint64_t I = 0; I < NumConstants; I++)
+    Consts.push_back(read<uint64_t>(&SMData));
+
+  // Records.
+  for (uint64_t I = 0; I < NumRecords; I++) {
+    uint64_t ID = read<uint64_t>(&SMData);
+    uint32_t InstrOff = read<uint32_t>(&SMData);
+    read<uint16_t>(&SMData); // skip reserved.
+    uint16_t NumLocs = read<uint16_t>(&SMData);
+
+    vector<SMLoc> Locs;
+    for (uint16_t J = 0; J < NumLocs; J++) {
+      uint8_t Where = read<uint8_t>(&SMData);
+      read<uint8_t>(&SMData); // Skip reserved.
+      uint16_t Size = read<uint16_t>(&SMData);
+      uint16_t RegNum = read<uint16_t>(&SMData);
+      read<uint16_t>(&SMData); // Skip reserved.
+      int32_t OffsetOrSmallConst = read<int32_t>(&SMData);
+      Locs.push_back({StorageClass(Where), Size, RegNum, OffsetOrSmallConst});
+    }
+
+    alignTo(&SMData, 8);     // Pad if necessary.
+    read<uint16_t>(&SMData); // Skip reserved.
+
+    // Liveouts.
+    uint16_t NumLiveOuts = read<uint16_t>(&SMData);
+    vector<SMLiveOut> LiveOuts;
+    for (uint64_t I = 0; I < NumLiveOuts; I++) {
+      uint16_t RegNum = read<uint16_t>(&SMData);
+      read<uint8_t>(&SMData); // Skip reserved.
+      uint8_t Size = read<uint8_t>(&SMData);
+      LiveOuts.push_back({RegNum, Size});
+    }
+
+    alignTo(&SMData, 8); // Pad if necessary.
+
+    Recs.insert({ID, {InstrOff, Locs, LiveOuts}});
+  }
+}
+
+SMRec *StackMapParser::getRecord(uint64_t ID) {
+  map<uint64_t, SMRec>::iterator It = Recs.find(ID);
+  if (It == Recs.end())
+    return nullptr;
+  return &It->second;
+}
+
+string getStorageClassName(StorageClass C) {
+  switch (C) {
+  case Reg:
+    return "Register";
+  case Direct:
+    return "Direct";
+  case Indirect:
+    return "Indirect";
+  case Const:
+    return "Constant";
+  case ConstIndex:
+    return "ConstIndex";
+  default:
+    errx(EXIT_FAILURE, "unreachable");
+  }
+}
---
+++ MultiSource/UnitTests/Stackmaps/StackMapParser.h
@@ -0,0 +1,75 @@
+#ifndef TEST_SUITE_STACKMAP_PARSER_H
+#define TEST_SUITE_STACKMAP_PARSER_H
+
+#include <inttypes.h>
+
+#include <assert.h>
+#include <elf.h>
+#include <err.h>
+#include <fcntl.h>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
+using namespace std;
+
+const uint8_t SMVersion = 3;
+
+enum StorageClass {
+  Reg = 1,
+  Direct = 2,
+  Indirect = 3,
+  Const = 4,
+  ConstIndex = 5
+};
+
+struct SMFunc {
+  uint64_t Addr;
+  uint64_t Size;
+  uint64_t RecCt;
+};
+
+struct SMLoc {
+  StorageClass Where;
+  uint16_t Size;
+  uint16_t RegNum;
+  int32_t OffsetOrSmallConst;
+};
+
+struct SMLiveOut {
+  uint16_t RegNo;
+  uint8_t Size;
+};
+
+struct SMRec {
+  uint32_t InstrOff;
+  vector<SMLoc> Locs;
+  vector<SMLiveOut> LiveOuts;
+};
+
+// A simple (copying) Stackmap parser.
+// https://llvm.org/docs/StackMaps.html#stack-map-format
+class StackMapParser {
+private:
+  vector<SMFunc> Funcs;
+  vector<uint64_t> Consts;
+  map<uint64_t, SMRec> Recs;
+
+  template <typename T> T read(char **SMData);
+  void alignTo(char **SMData, size_t To);
+
+public:
+  StackMapParser(char *SMData);
+  SMRec *getRecord(uint64_t ID);
+};
+
+string getStorageClassName(StorageClass C);
+
+#endif // TEST_SUITE_STACKMAP_PARSER_H
